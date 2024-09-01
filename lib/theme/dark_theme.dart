2d9eb1152330e21c9ea9e8d559518ce8870d7f55
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-09-01 14:21:13 +0300

    update dark theme color value

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import '../theme/light_theme.dart';
 
+const darkThemeTextColor = Colors.white;
+
 final darkColorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
   seedColor: seedColor,
@@ -12,20 +14,50 @@ final darkTheme = ThemeData().copyWith(
   scaffoldBackgroundColor: Colors.grey[800],
   colorScheme: darkColorScheme,
   textTheme: textTheme.copyWith(
-    titleSmall: textTheme.titleSmall!.copyWith(color: Colors.white),
-    titleMedium: textTheme.titleMedium!.copyWith(color: Colors.white),
-    titleLarge: textTheme.titleLarge!.copyWith(color: Colors.white),
-    bodyLarge: textTheme.bodyLarge!.copyWith(color: Colors.white),
-    bodyMedium: textTheme.bodyMedium!.copyWith(color: Colors.white),
-    bodySmall: textTheme.bodySmall!.copyWith(color: Colors.white),
-    labelSmall: textTheme.labelSmall!.copyWith(color: Colors.white),
-    labelLarge: textTheme.labelLarge!.copyWith(color: Colors.white),
-    labelMedium: textTheme.labelMedium!.copyWith(color: Colors.white),
-    displayLarge: textTheme.displayLarge!.copyWith(color: Colors.white),
-    displayMedium: textTheme.displayMedium!.copyWith(color: Colors.white),
-    displaySmall: textTheme.displaySmall!.copyWith(color: Colors.white),
-    headlineLarge: textTheme.headlineLarge!.copyWith(color: Colors.white),
-    headlineMedium: textTheme.headlineMedium!.copyWith(color: Colors.white),
-    headlineSmall: textTheme.headlineSmall!.copyWith(color: Colors.white),
+    titleSmall: textTheme.titleSmall!.copyWith(
+      color: darkThemeTextColor,
+    ),
+    titleMedium: textTheme.titleMedium!.copyWith(
+      color: darkThemeTextColor,
+    ),
+    titleLarge: textTheme.titleLarge!.copyWith(
+      color: darkThemeTextColor,
+    ),
+    bodyLarge: textTheme.bodyLarge!.copyWith(
+      color: darkThemeTextColor,
+    ),
+    bodyMedium: textTheme.bodyMedium!.copyWith(
+      color: darkThemeTextColor,
+    ),
+    bodySmall: textTheme.bodySmall!.copyWith(
+      color: darkThemeTextColor,
+    ),
+    labelSmall: textTheme.labelSmall!.copyWith(
+      color: darkThemeTextColor,
+    ),
+    labelLarge: textTheme.labelLarge!.copyWith(
+      color: darkThemeTextColor,
+    ),
+    labelMedium: textTheme.labelMedium!.copyWith(
+      color: darkThemeTextColor,
+    ),
+    displayLarge: textTheme.displayLarge!.copyWith(
+      color: darkThemeTextColor,
+    ),
+    displayMedium: textTheme.displayMedium!.copyWith(
+      color: darkThemeTextColor,
+    ),
+    displaySmall: textTheme.displaySmall!.copyWith(
+      color: darkThemeTextColor,
+    ),
+    headlineLarge: textTheme.headlineLarge!.copyWith(
+      color: darkThemeTextColor,
+    ),
+    headlineMedium: textTheme.headlineMedium!.copyWith(
+      color: darkThemeTextColor,
+    ),
+    headlineSmall: textTheme.headlineSmall!.copyWith(
+      color: darkThemeTextColor,
+    ),
   ),
 );
