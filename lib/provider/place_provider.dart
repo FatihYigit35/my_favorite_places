4cdf41c9200a1adb301df3147fcbe9aae4e19080
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-09-07 14:05:47 +0300

    added sqlite

--- lib/provider/place_provider.dart
+++ lib/provider/place_provider.dart
@@ -1,14 +1,87 @@
+import 'dart:io';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_favorite_places/model/place_location.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 import '../model/place.dart';
+
+Future<Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  final db = await sql.openDatabase(
+    path.join(dbPath, 'places.sqlite'),
+    onCreate: (db, version) {
+      return db.execute(
+          'CREATE TABLE favorite_places(id TEXT PRIMARY KEY, title TEXT, description TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
+    },
+    version: 1,
+  );
+  return db;
+}
 
 class PlaceNotifier extends StateNotifier<List<Place>> {
   PlaceNotifier() : super(const []);
 
-  void addPlace(Place place) {
+  Future<void> loadPlaces() async {
+    final db = await _getDatabase();
+    final data = await db.query('favorite_places');
+
+    final places = data
+        .map(
+          (e) => Place(
+            id: e['id'] as String,
+            title: e['title'] as String,
+            description: e['description'] as String,
+            image: File(e['image'] as String),
+            location: PlaceLocation(
+              latitude: e['lat'] as double,
+              longitude: e['lng'] as double,
+              address: e['address'] as String,
+            ),
+          ),
+        )
+        .toList();
+
+    state = places;
+  }
+
+  void addPlace(Place place) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+
+    if (place.image == null) {
+      return;
+    }
+
+    final image = place.image!;
+
+    final fileName = path.basename(image.path);
+
+    final coppiedImage = await image.copy('${appDir.path}/$fileName');
+
+    place.image = coppiedImage;
+
+    final db = await _getDatabase();
+
+    db.insert('favorite_places', {
+      'id': place.id,
+      'title': place.title,
+      'description': place.description,
+      'image': place.image?.path,
+      'lat': place.location?.latitude,
+      'lng': place.location?.longitude,
+      'address': place.location?.address,
+    });
+
     final newList = state.toList();
     newList.add(place);
     state = newList;
+  }
+
+  List<Place> getPlaces() {
+    return state;
   }
 }
 
--- lib/screen/favorite_places_screen.dart
+++ lib/screen/favorite_places_screen.dart
@@ -2,35 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../provider/place_provider.dart';
-import '../widget/place_list_item.dart';
+import '../widget/places_list.dart';
 import './new_place_screen.dart';
 
-class FavoritePlacesScreen extends ConsumerWidget {
+class FavoritePlacesScreen extends ConsumerStatefulWidget {
   const FavoritePlacesScreen({super.key});
+  @override
+  ConsumerState<FavoritePlacesScreen> createState() =>
+      _FavoritePlacesScreenState();
+}
+
+class _FavoritePlacesScreenState extends ConsumerState<FavoritePlacesScreen> {
+  late Future<void> _placesFuture;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(placeProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final places = ref.watch(placeProvider);
-
-    Widget content = const Center(
-      child: Text(
-        'No place added yet',
-        style: TextStyle(
-          fontSize: 20,
-        ),
-      ),
-    );
-
-    if (places.isNotEmpty) {
-      content = ListView.builder(
-        itemCount: places.length,
-        itemBuilder: (context, index) {
-          return PlaceListItem(
-            place: places[index],
-          );
-        },
-      );
-    }
 
     return Scaffold(
         appBar: AppBar(
@@ -46,6 +39,13 @@ class FavoritePlacesScreen extends ConsumerWidget {
             ),
           ],
         ),
-        body: content);
+        body: FutureBuilder(
+            future: _placesFuture,
+            builder: (context, snapshot) =>
+                snapshot.connectionState == ConnectionState.waiting
+                    ? const Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : PlacesList(places: places)));
   }
 }
